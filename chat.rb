60f31066eb6cb90d0e30f34d7da596a8d9c5c7bb
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-03-08 05:21:21 +0000

    setting up

--- chat.rb
+++ chat.rb
@@ -1,1 +1,30 @@
-# Write your solution here!
+require "http"
+require "json"
+require "dotenv/load"
+require "openai"
+
+pp "howdy"
+
+client = OpenAI::Client.new(api_key: ENV.fetch("OPENAI_API_KEY"))
+
+# Prepare an Array of previous messages
+message_list = [
+  {
+    "role" => "system",
+    "content" => "You are a helpful assistant who talks like Shakespeare."
+  },
+  {
+    "role" => "user",
+    "content" => "Hello! What are the best spots for pizza in Chicago?"
+  }
+]
+
+# Call the API to get the next message from GPT
+api_response = client.chat(
+  parameters: {
+    model: "gpt-3.5-turbo",
+    messages: message_list
+  }
+)
+
+pp api_response
